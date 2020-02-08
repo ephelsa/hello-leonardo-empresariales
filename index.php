7485f6635b1a193c050391d56c796b9be507b4d0
Leonardo A. Perez Castilla <ephelsa@hotmail.com>
2020-02-08 09:29:48 -0500

    Deploy empresariales

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+  $name = 'Leonardo';
+  $message = "Hello, $name";
+?>
+
+<html>
+  <head>
+    <title><php? echo "$message" ?></title>
+  </head>
+  <body>
+    <?php echo "$message"; ?>
+  </body>
+</html>
